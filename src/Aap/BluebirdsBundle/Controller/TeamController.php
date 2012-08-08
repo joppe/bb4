a5754b2a080dcf9eb3bf3d4bef32708a6bc83f8b
Joppe Aarts <joppe@apestaartje.info>
2012-08-08 23:18:25 +0200

    sass sprite mixin added

--- src/Aap/BluebirdsBundle/Controller/TeamController.php
+++ src/Aap/BluebirdsBundle/Controller/TeamController.php
@@ -6,10 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class TeamController extends Controller {
-    public function indexAction($name) {
+    /**
+     * Display the team homepage
+     *
+     * @param string $slug
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction($slug) {
 //        return new Response('team ' . $name);
         return $this->render('AapBluebirdsBundle:Team:index.html.twig', array(
-            'team_name' => $name
+            'team_name' => 'Blue Birds 4'
         ));
     }
 }
---
+++ src/Aap/BluebirdsBundle/Entity/Player.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Aap\BluebirdsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="player")
+ */
+class Player {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $middle_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $street;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $zip;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $city;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set first_name
+     *
+     * @param string $firstName
+     * @return Player
+     */
+    public function setFirstName($firstName)
+    {
+        $this->first_name = $firstName;
+    
+        return $this;
+    }
+
+    /**
+     * Get first_name
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * Set middle_name
+     *
+     * @param string $middleName
+     * @return Player
+     */
+    public function setMiddleName($middleName)
+    {
+        $this->middle_name = $middleName;
+    
+        return $this;
+    }
+
+    /**
+     * Get middle_name
+     *
+     * @return string 
+     */
+    public function getMiddleName()
+    {
+        return $this->middle_name;
+    }
+
+    /**
+     * Set last_name
+     *
+     * @param string $lastName
+     * @return Player
+     */
+    public function setLastName($lastName)
+    {
+        $this->last_name = $lastName;
+    
+        return $this;
+    }
+
+    /**
+     * Get last_name
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Player
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set telephone
+     *
+     * @param string $telephone
+     * @return Player
+     */
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+    
+        return $this;
+    }
+
+    /**
+     * Get telephone
+     *
+     * @return string 
+     */
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return Player
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+    
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set zip
+     *
+     * @param string $zip
+     * @return Player
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+    
+        return $this;
+    }
+
+    /**
+     * Get zip
+     *
+     * @return string 
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Player
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+}
